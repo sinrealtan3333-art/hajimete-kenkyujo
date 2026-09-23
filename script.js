0b3b45a8d3c7760f296802351e09afedeee462c6
sinrealtan3333-art <sinrealtan3333@gmail.com>
2026-09-24 00:00:28 +0900

    Refine smooth navigation

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-document.querySelectorAll('a[href^="#"]').forEach(a=>a.addEventListener("click",e=>{const t=document.querySelector(a.getAttribute("href"));if(t){e.preventDefault();t.scrollIntoView({behavior:"smooth"})}}));
+document.querySelectorAll('a[href^="#"]').forEach(a=>a.addEventListener('click',e=>{const h=a.getAttribute('href');const t=h?document.querySelector(h):null;if(t){e.preventDefault();t.scrollIntoView({behavior:'smooth',block:'start'})}}));
